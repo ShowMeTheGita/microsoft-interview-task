726c8e28d6220317bc7bff0ab1040ec92672d86e
Guilherme Silva <guilherme.m.m.f.silva@gmail.com>
2022-03-21 23:15:19 +0000

    add basic express config, barebones html

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const msal = require('@azure/msal-node');
+const path = require('path');
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+// Create Express App and Routes
+const app = express();
+
+app.listen(SERVER_PORT, () => console.log(`Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`))
+
+
+// Config //
+
+
+const clientId = "8142c14f-9866-43e9-a0d0-e93f773f8503"
+const authorityId = "560a8f3a-5282-4c2b-ac62-51234b1428b5"
+const clientSecret = "EIr7Q~553A_6ywyrAiDcq5pRbsBH8rp6EUu4m"
+
+const config = {
+    auth: {
+      clientId: clientId,
+      authority: `https://login.microsoftonline.com/${authorityId}`,
+      clientSecret: clientSecret
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+                },
+                piiLoggingEnabled: false,
+                logLevel: msal.LogLevel.Verbose,
+            }
+        }
+};
+
+
+const cca = new msal.ConfidentialClientApplication(config);
+
+app.get('/login', (req, res) => {
+    const authCodeUrlParameters = {
+        scopes: ["user.read"],
+       redirectUri: "http://localhost:3000/redirect",
+    };
+
+    cca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+        res.redirect(response);
+    }).catch((error) => console.log(JSON.stringify(error)));
+});
+
+
+app.get('/index.js', function(req, res){
+    res.sendFile(__dirname + '/index.js');
+});
+
+
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        scopes: ["user.read"],
+        redirectUri: "http://localhost:3000/redirect",
+    };
+
+    cca.acquireTokenByCode(tokenRequest).then((response) => {
+        console.log("\nResponse: \n:", response);
+        res.sendFile(__dirname + '/index.html')
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+    });
+});
