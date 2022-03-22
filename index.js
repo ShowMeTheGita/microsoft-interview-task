98e130f6824cf4f93f2a4686c73f70a5b3249f93
Guilherme Silva <guilherme.m.m.f.silva@gmail.com>
2022-03-22 21:38:42 +0000

    first endpoint for interracting with graph

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ window.onload = function () {
     getUserInfo()
 }
 
-let userInfo;
-
-function getUserInfo() {
-    let toSend = {}
-    url = "http://localhost:3000/loggedInUser"
-    fetch(url).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        let email = data.account.username;
-        let name = email.substring(0, email.lastIndexOf("@"));
-        document.getElementById("welcome-message").innerHTML = "Hello, " + name
-    })
-}
-
-
 function welcomeMessage() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
@@ -26,7 +11,28 @@ function welcomeMessage() {
     url = "http://localhost:3000/loggedInUser"
     xmlHttp.open("GET", url, true); // true for asynchronous 
     xmlHttp.send(null);
-    
 }
+
+let userInfo;
+function getUserInfo() {
+    url = "http://localhost:3000/loggedInUser"
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        let email = data.username;
+        let name = email.substring(0, email.lastIndexOf("@"));
+        document.getElementById("welcome-message").innerHTML = "Hello, " + name
+    })
+}
+
+function callGraphMe() {
+    url = "http://localhost:3000/getUserDetailsGraph"
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data)
+    })
+}
+
 
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+///////////////// Express Config /////////////////////
+
 const express = require("express");
 const msal = require('@azure/msal-node');
 const path = require('path');
@@ -10,7 +12,7 @@ const app = express();
 app.listen(SERVER_PORT, () => console.log(`Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`))
 
 
-// Config //
+///////////////// Msal Config ////////////////
 
 
 const clientId = "8142c14f-9866-43e9-a0d0-e93f773f8503"
@@ -34,9 +36,12 @@ const config = {
         }
 };
 
+/////////////////////////////////////////////////////////////////////////////
 
 const cca = new msal.ConfidentialClientApplication(config);
+const baseGraphUrl = "https://graph.microsoft.com/v1.0/";
 let loggedInUser = {};
+let accessToken = "";
 
 app.get('/login', (req, res) => {
     const authCodeUrlParameters = {
@@ -54,7 +59,6 @@ app.get('/index.js', function(req, res){
     res.sendFile(__dirname + '/index.js');
 });
 
-
 app.get('/loggedInUser', (req, res) => {
     res.json(loggedInUser);
 });
@@ -68,10 +72,30 @@ app.get('/redirect', (req, res) => {
 
     cca.acquireTokenByCode(tokenRequest).then((response) => {
         console.log("\nResponse: \n:", response);
-        loggedInUser = response;
+        username = response.account.username;
+        loggedInUser = {'username': username};
+        accessToken = response.accessToken;
         res.sendFile(__dirname + '/index.html')
     }).catch((error) => {
         console.log(error);
         res.status(500).send(error);
     });
+});
+
+app.get('/getUserDetailsGraph', (req, res) =>  {
+
+    let graphEndpoint = "me"
+    let url = baseGraphUrl + graphEndpoint
+
+    fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            'Authorization': `Bearer ${tokenResponse.accessToken}`
+        })
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        res.json(data)
+    });
+
 });
